2f1423e9204e94651259b9403720015b239031e6
Marcus Wong <marcusthomaswong@gmail.com>
2010-03-23 08:34:24 +0000

    wongm theme - reorder so no querystring required

--- everything.php
+++ everything.php
@@ -1,9 +1,29 @@
 <?php
 /*
+ * draw all dynamic albums in the gallery
+ * 
+ */
+if(isset($_REQUEST['by-theme']))
+{
+	$pageTitle = ' - Albums by theme';
+	$rssType = 'Gallery';
+	$rssTitle = 'Recent uploads';
+	
+	include_once('header.php'); 
+?>
+<table class="headbar">
+	<tr><td><a href="<?=getGalleryIndexURL();?>" title="Gallery Index"><?=getGalleryTitle();?></a> &raquo;
+	<a href="/gallery" title="Albums by theme">Albums by theme</a></td>
+	<td><?printSearchForm();?></td></tr>
+</table>
+<?
+	drawIndexAlbums('dynamiconly');
+}
+/*
  * draw all albums in the gallery
  * 
  */
-if(isset($_REQUEST['all-albums']))
+else
 {
 	$pageTitle = ' - All albums';
 	$rssType = 'Gallery';
@@ -42,26 +62,6 @@ if(isset($_REQUEST['all-albums']))
 </div>
 <? 
 	}
-}
-/*
- * draw all dynamic albums in the gallery
- * 
- */
-else if(isset($_REQUEST['by-theme']))
-{
-	$pageTitle = ' - Albums by theme';
-	$rssType = 'Gallery';
-	$rssTitle = 'Recent uploads';
-	
-	include_once('header.php'); 
-?>
-<table class="headbar">
-	<tr><td><a href="<?=getGalleryIndexURL();?>" title="Gallery Index"><?=getGalleryTitle();?></a> &raquo;
-	<a href="/gallery" title="Albums by theme">Albums by theme</a></td>
-	<td><?printSearchForm();?></td></tr>
-</table>
-<?
-	drawIndexAlbums('dynamiconly');
 }
 include_once('footer.php');
 ?>
