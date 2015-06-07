bf3093d9a3a0582f6a2d149511117d8903c4eeac
Marcus Wong <marcusthomaswong@gmail.com>
2015-06-07 13:54:19 +1000

    ensure 'resize image' page works for new 1920px standard

--- recent-resize.php
+++ recent-resize.php
@@ -1,6 +1,6 @@
 <?php $startTime = array_sum(explode(" ",microtime())); if (!defined('WEBPATH')) die(); 
 
-$where = "(i.height != 1024 AND i.width != 683) OR (i.width != 1024 AND i.height != 683)";
+$where = "(i.height = 1024 AND i.width != 683) OR (i.width = 1024 AND i.height != 683) OR (i.height = 1920 AND i.width != 1280) OR (i.width = 1920 AND i.height != 1280)";
 setCustomPhotostream($where);
 require_once('uploads-base.php');
 ?>
