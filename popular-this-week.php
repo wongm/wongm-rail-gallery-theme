5da28d193b3ce1a6f896e66a9ab7a5cfd56294b5
Marcus Wong <marcusthomaswong@gmail.com>
2015-06-07 13:23:06 +1000

    Fix issue where hitcounter for newly uploaded images was not initialised

--- popular-this-week.php
+++ popular-this-week.php
@@ -7,6 +7,14 @@ if ( zp_loggedin() ) {
 	$dateLastWeek = date("Y-m-d", $time - (60*60*24*6.5));
 	$dateLastMonth = date("Y-m-d", $time - (60*60*24*30));
 	
+	// ensure all new images have the reset date set to today
+	$sqlToReset = "UPDATE " . prefix('images') . " SET hitcounter_month_reset = '$dateCurrent' WHERE hitcounter_month_reset IS NULL";
+	query($sqlToReset);
+	
+	$sqlToReset = "UPDATE " . prefix('images') . " SET hitcounter_week_reset = '$dateCurrent' WHERE hitcounter_week_reset IS NULL";
+	query($sqlToReset);
+	
+	// reset the week / month hitcounter if required
 	$sqlToReset = "UPDATE " . prefix('images') . " SET hitcounter_month_reset = '$dateCurrent', hitcounter_month = 0 WHERE hitcounter_month_reset < '$dateLastMonth'";
 	query($sqlToReset);
 	
