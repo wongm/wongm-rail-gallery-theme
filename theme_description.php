5ee13d0ecec07a0645db61c59fa271f85f9a37a7
Marcus Wong <marcusthomaswong@gmail.com>
2015-06-07 13:54:59 +1000

    Update version

--- theme_description.php
+++ theme_description.php
@@ -2,7 +2,7 @@
 // Zenphoto theme definition file
 $theme_description['name'] = 'Wongm\'s Rail Gallery';
 $theme_description['author'] = gettext('<a href="http://wongm.com" target="_blank">Marcus Wong</a>');
-$theme_description['version'] = '2.6.5';
-$theme_description['date'] = 'June 6, 2013';
+$theme_description['version'] = '2.8';
+$theme_description['date'] = 'June 7, 2015';
 $theme_description['desc'] = gettext("desc::Variant of the Rail Geelong theme, itself based upon the 'Sub-Albums Testing Theme' by Tristan Harward. Now resets the last visited dates when in admin 'recent uploads' or 'popular' views.");
 ?>
