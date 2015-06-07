89618ce8c0ee8758db0e803fd3a99f81f6e3bde1
Marcus Wong <marcusthomaswong@gmail.com>
2015-06-07 13:35:49 +1000

    Ensure links are added to admin menu

--- functions.php
+++ functions.php
@@ -82,6 +82,52 @@ $popularImageText['wagons']['order'] = "i.mtime DESC";
 $popularImageText['wagons']['where'] = "folder LIKE 'wagons%'";
 $popularImageText['wagons']['subtext'] = 'Sorted by upload date to the site (not when they were taken).';
 
+zp_register_filter('checkPageValidity', 'wongmTheme::checkLinks');
+zp_register_filter('admin_toolbox_album', 'wongmTheme::addAlbumLink');
+zp_register_filter('admin_toolbox_global', 'wongmTheme::addGlobalLink');
+
+/**
+ * Plugin option handling class
+ *
+ */
+class wongmTheme {
+	
+	static function checkLinks($count, $gallery_page, $page) {
+    	switch (stripSuffix($gallery_page))
+    	{
+        	case 'archive':
+        	case 'everything':
+        	case 'popular-all-time':
+        	case 'popular-by-ratings':
+        	case 'popular-ratings':
+        	case 'popular-this-month':
+        	case 'popular-this-week':
+        	case 'recent-albums':
+        	case 'recent-duplicates':
+        	case 'recent-resize':
+        	case 'recent-uncaptioned':
+        	case 'recent-uncaptioned-albums':
+        	case 'recent-uploads':
+        	case 'recent-wagons':
+        	case 'rss-updates':
+        	    return true;
+		}
+	}
+
+    static function addAlbumLink($albumname) {
+    }
+    
+    static function addGlobalLink() {
+    	echo "<li>";
+    	printLinkHTML(WEBPATH.'/page/recent-uncaptioned', 'Uncaptioned images', NULL, NULL, NULL);
+    	echo "</li><li>";
+    	printLinkHTML(WEBPATH.'/page/recent-uncaptioned-albums', 'Uncaptioned albums', NULL, NULL, NULL);
+    	echo "</li><li>";
+    	printLinkHTML(WEBPATH.'/page/recent-duplicates', 'Duplicate images', NULL, NULL, NULL);
+    	echo "</li>";
+    }
+}
+
 function printFacebookTag()
 {
 	$protocol = SERVER_PROTOCOL;
