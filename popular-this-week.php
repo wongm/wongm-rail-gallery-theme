de3775ff9c2b454b46b276bcfd71121ec868f3a9
Marcus Wong <marcusthomaswong@gmail.com>
2016-04-20 12:49:56 +1000

    Refactor 'popular this week' page to reset both album and image hitcounters

--- popular-this-week.php
+++ popular-this-week.php
@@ -2,24 +2,9 @@
 
 // reset hitcounters
 if ( zp_loggedin() ) {
-	$time = time();
-	$dateCurrent = date("Y-m-d");
-	$dateLastWeek = date("Y-m-d", $time - (60*60*24*6.5));
-	$dateLastMonth = date("Y-m-d", $time - (60*60*24*30));
-	
-	// ensure all new images have the reset date set to today
-	$sqlToReset = "UPDATE " . prefix('images') . " SET hitcounter_month_reset = '$dateCurrent' WHERE hitcounter_month_reset IS NULL";
-	query($sqlToReset);
-	
-	$sqlToReset = "UPDATE " . prefix('images') . " SET hitcounter_week_reset = '$dateCurrent' WHERE hitcounter_week_reset IS NULL";
-	query($sqlToReset);
-	
-	// reset the week / month hitcounter if required
-	$sqlToReset = "UPDATE " . prefix('images') . " SET hitcounter_month_reset = '$dateCurrent', hitcounter_month = 0 WHERE hitcounter_month_reset < '$dateLastMonth'";
-	query($sqlToReset);
-	
-	$sqlToReset = "UPDATE " . prefix('images') . " SET hitcounter_week_reset = '$dateCurrent', hitcounter_week = 0 WHERE hitcounter_week_reset < '$dateLastWeek'";
-	query($sqlToReset);
+    
+    resetDates('images');
+    resetDates('albums');
 }
 
 $key = 'this-week';
@@ -27,4 +12,27 @@ $popularImageText['key'] = $key;
 setCustomPhotostream($popularImageText[$key]['where'], "", $popularImageText[$key]['order']);
 
 require_once('uploads-base.php');
+
+function resetDates($type)
+{
+	$time = time();
+	$dateCurrent = date("Y-m-d");
+	$dateLastWeek = date("Y-m-d", $time - (60*60*24*6.5));
+	$dateLastMonth = date("Y-m-d", $time - (60*60*24*30));
+	
+	// ensure all new images have the reset date set to today
+	$sqlToReset = "UPDATE " . prefix($type) . " SET hitcounter_month_reset = '$dateCurrent' WHERE hitcounter_month_reset IS NULL";
+	query($sqlToReset);
+	
+	$sqlToReset = "UPDATE " . prefix($type) . " SET hitcounter_week_reset = '$dateCurrent' WHERE hitcounter_week_reset IS NULL";
+	query($sqlToReset);
+	
+	// reset the week / month hitcounter if required
+	$sqlToReset = "UPDATE " . prefix($type) . " SET hitcounter_month_reset = '$dateCurrent', hitcounter_month = 0 WHERE hitcounter_month_reset < '$dateLastMonth'";
+	query($sqlToReset);
+	
+	$sqlToReset = "UPDATE " . prefix($type) . " SET hitcounter_week_reset = '$dateCurrent', hitcounter_week = 0 WHERE hitcounter_week_reset < '$dateLastWeek'";
+	query($sqlToReset);
+}
+
 ?>
