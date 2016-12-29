065a4553f7dfcb358014d6daf949006ba70d58bc
Marcus Wong <marcusthomaswong@gmail.com>
2016-12-29 21:12:41 +1100

    How did I forget the viewport?

--- header.php
+++ header.php
@@ -49,6 +49,7 @@ $galleryImageAlbumCountMessage = buildGalleryImageAlbumCountMessage();
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 <script type="text/javascript" src="<?= $_zp_themeroot ?>/js/slimbox2.js"></script>
 <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="author" content="Marcus Wong" />
 <meta name="keywords" content="railways train geelong Victoria Australia photos photographs images" />
 <?php
