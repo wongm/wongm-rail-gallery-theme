8edaadd203d1c8a9ff10a67a7d27b5b255631ded
marcusw <marcusw@pageuppeople.com>
2016-12-30 13:10:22 +1100

    Make image detail page responsive

--- image.php
+++ image.php
@@ -16,8 +16,8 @@ include_once('header.php'); ?>
 </div>
 
 <div id="viewImage">  
-	<img id="mainImage" src="<? echo getDefaultSizedImage() ?>" alt="<?=getImageTitle();?>" width="<?=getDefaultWidth();?>" height="<?=getDefaultHeight();?>" style="left: 50%; margin-left: -<?=getDefaultWidth() / 2;?>px;" />
-	<div style="position: relative; left: 50%; margin-left: -<?=getDefaultWidth() / 2;?>px; width: <?=getDefaultWidth();?>px; height: <?=getDefaultHeight();?>px;">
+	<img id="mainImage" src="<? echo getDefaultSizedImage() ?>" alt="<?=getImageTitle();?>" width="<?=getDefaultWidth();?>" height="<?=getDefaultHeight();?>" style="margin-left: -<?=getDefaultWidth() / 2;?>px;" />
+	<div id="imageOverlay" style="margin-left: -<?=getDefaultWidth() / 2;?>px; width: <?=getDefaultWidth();?>px; height: <?=getDefaultHeight();?>px;">
     <?php if (hasPrevImage()) { ?>
 		<a href="<?=getPrevImageURL();?>" id="lbPrevLink" style="height: <?=getDefaultHeight();?>px;"></a>
 	<?php } if (hasNextImage()) { ?>
@@ -28,26 +28,22 @@ include_once('header.php'); ?>
 		<a id="showImage" href="<?=getFullImageURL();?>" rel="lightbox" title="<?=getImageTitle();?>">View full sized (<?=getFullWidth()?>px by <?=getFullHeight()?>px)</a>
 	</p>
 <?php 
-	printEXIFData() ; 
-  
-   //printjCarouselThumbNav(NULL, 250, 250,0,0,false,false);
+	printEXIFData();
   
   ?>
 </div>
 <?php if (hasPrevImage() or hasNextImage()) { ?>    
-<table class="pagelist"><tr><td>
+<div class="pagelist"><div class="prev wrapper">
     <?php if (hasPrevImage()) { ?>
     <a id="prevLink" class="prev" href="<?=getPrevImageURL();?>" title="Previous Image"><span>&laquo;</span> Previous</a>
-    </td><td>
     <a class="prevIcon" href="<?=getPrevImageURL();?>" title="<?=getPrevImageTitle();?>"><img src="<?=getPrevImageThumb();?>" alt="<?=getPrevImageTitle();?>" /></a>
     <?php } else { echo "</td><td>"; } ?>
-    </td><td>
+    </div><div class="next wrapper">
     <?php if (hasNextImage()) { ?>
     <a class="nextIcon" href="<?=getNextImageURL();?>" title="<?=getNextImageTitle();?>"><img src="<?=getNextImageThumb();?>" alt="<?=getNextImageTitle();?>"/></a>
-    </td><td>
     <a id="nextLink" class="next" href="<?=getNextImageURL();?>" title="Next Image">Next <span>&raquo;</span></a>
     <?php } else { echo "</td><td>"; } ?>
-</td></tr></table>
+</div></div>
 <?php } ?>
 <?
 if(function_exists("printImageMarkupFields"))
