68ec05fec96379a4f41d81f7faffaa3315206ba0
marcusw <marcusw@pageuppeople.com>
2017-02-17 15:56:31 +1100

    Ensure 'resize images' page skips wagon photos

--- recent-resize.php
+++ recent-resize.php
@@ -2,7 +2,7 @@
 
 $popularImageText['key'] = 'resize';
 
-$where = "(i.height = 1024 AND i.width != 683) OR (i.width = 1024 AND i.height != 683) OR (i.height = 1920 AND i.width != 1280) OR (i.width = 1920 AND i.height != 1280)";
+$where = "((i.height = 1024 AND i.width != 683) OR (i.width = 1024 AND i.height != 683) OR (i.height = 1920 AND i.width != 1280) OR (i.width = 1920 AND i.height != 1280)) AND a.folder NOT LIKE 'wagons%'";
 setCustomPhotostream($where);
 require_once('uploads-base.php');
 ?>
