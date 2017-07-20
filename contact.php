d01ed0d49b3c1753d7eda41e5b201c529c7f515f
Marcus Wong <marcusthomaswong@gmail.com>
2017-07-20 15:45:12 +1000

    Switch over to built in Zenphoto comment form

--- contact.php
+++ contact.php
@@ -1,12 +1,21 @@
 <?php $startTime = array_sum(explode(" ",microtime())); if (!defined('WEBPATH')) die();
 
-$pageTitle = ' - Contact me';
+$pageTitle = ' - About me';
 include_once('header.php'); ?>
 <table class="headbar">
-	<tr><td><a href="<?=getGalleryIndexURL();?>" title="Gallery Index"><?=getGalleryTitle();?></a> &raquo; Contact me
+	<tr><td><a href="<?=getGalleryIndexURL();?>" title="Gallery Index"><?=getGalleryTitle();?></a> &raquo; About me
 	</td><td><?printSearchForm();?></td></tr>
 </table>
+<style>
+#mailform { 
+    border: 1px solid grey;
+    padding: 10px;
+}
+label { display: block }
+</style>
 <?php
+$page = new ZenpagePage('About-Me');
+echo $page->getContent();
 printContactForm();
 include_once('footer.php'); 
 ?>
