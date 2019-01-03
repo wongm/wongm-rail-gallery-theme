e8692273aa2bbafa9dd193539afdc487efcfbf68
marcusw <marcusw@pageuppeople.com>
2019-01-03 17:45:15 +1100

    Tweak bus folder filter logic

--- recent-trains.php
+++ recent-trains.php
@@ -7,7 +7,7 @@ $popularImageText['trains']['title'] = 'Recent uploads - Trains';
 $popularImageText['trains']['text'] = 'Recent train uploads';
 $popularImageText['trains']['subtext'] = 'Trains and railway infrastructure';
 $popularImageText['trains']['type'] = 'recent';
-$popularImageText['trains']['where'] = "folder NOT LIKE '%bus%' AND folder NOT LIKE '%tram%' AND folder NOT LIKE '%light-rail%' AND folder NOT LIKE 'wagons%' AND folder NOT LIKE 'road-coaches'";
+$popularImageText['trains']['where'] = "folder NOT LIKE '%tram%' AND folder NOT LIKE '%light-rail%' AND folder NOT LIKE 'wagons%' AND a.id NOT IN (SELECT `objectid` FROM zen_obj_to_tag INNER JOIN zen_tags ON zen_obj_to_tag.`tagid` = zen_tags.`id` WHERE zen_obj_to_tag.`type` = 'albums' AND zen_tags.`name` = 'buses')";
 
 setCustomPhotostream($popularImageText[$key]['where']);
 
