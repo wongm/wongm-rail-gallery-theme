f88fc15b9ff6c5268284e0e17c9cb21bf8fc3be5
Marcus Wong <marcusthomaswong@gmail.com>
2019-04-09 20:48:29 +1000

    Address #2 - load the image path befoere resetting context

--- functions-gallery-formatting.php
+++ functions-gallery-formatting.php
@@ -638,16 +638,16 @@ function printMetadata($pageTitle)
 		
 		// makeImageCurrent can change $_zp_current_album and $_zp_current_image variable to child album
 		// save a local copy, so we can get THIS album back later
-		// also need to reset image to ensure that rest of Zenphoto does not get confused
 		$currentAlbum = $_zp_current_album;
 		$currentImage = $_zp_current_image;
 		$currentContext = get_context();
 		makeImageCurrent($_zp_current_album->getAlbumThumbImage());
+		$imagePath = getDefaultSizedImage();
+		
+		// now reset image to ensure that rest of Zenphoto does not get confused
 		$_zp_current_album = $currentAlbum;
 		$_zp_current_image = $currentImage;
 		set_context($currentContext);
-		
-		$imagePath = getDefaultSizedImage();
 		
 		if (strlen(getAlbumDesc()) > 0) {
 			$description = strip_tags(getAlbumDesc());
