041d8859499090218a6fc7c458f9e4d50af3ece6
Marcus Wong <marcusthomaswong@gmail.com>
2020-04-14 11:52:00 +1000

    Change 'albumsonly' mode to display albums as a grid

--- index.php
+++ index.php
@@ -11,8 +11,27 @@ if (getOption('wongm_frontpage_mode') == 'albumsonly') {
 <span id="breadcrumb"><a href="<?php echo getGalleryIndexURL(); ?>" title="Gallery Index"><?php echo getGalleryTitle(); ?></a> &raquo; Welcome!
 </span><span id="righthead"><?echo printSearchForm(); ?></span>
 </div>
+<div id="imagewrapper">
+	<div id="images">
 <?php
-    drawIndexAlbums('nodynamic');
+while (next_album(true))
+{
+?>
+<div class="image">
+	<div class="imagethumb"><a href="<?php echo getAlbumURL();?>" title="<?php echo getAlbumTitle();?>">
+		<?php printSizedAlbumThumbImage(getAlbumTitle()); ?>
+	</a></div>
+	<div class="imagetitle">
+		<h4><a href="<?php echo getAlbumURL();?>" title="<?php echo getAlbumTitle();?>"><?php echo getAlbumTitle(); ?></a></h4>
+		<?php echo printAlbumDesc(); ?>
+	</div>
+</div>
+<?php
+}
+?>
+	</div>
+</div>
+<?php
 	include_once('footer.php');
 } else {
     include_once('frontpage.php');
