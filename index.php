129bc35e6a9494c3db7eb8d609c6edfd17672163
Marcus Wong <marcusthomaswong@gmail.com>
2021-12-06 12:55:23 +1100

    Remove album display from front page

--- index.php
+++ index.php
@@ -11,27 +11,8 @@ if (getOption('wongm_frontpage_mode') == 'albumsonly') {
 <span id="breadcrumb"><a href="<?php echo getGalleryIndexURL(); ?>" title="Gallery Index"><?php echo getGalleryTitle(); ?></a> &raquo; Welcome!
 </span><span id="righthead"><?echo printSearchForm(); ?></span>
 </div>
-<div id="imagewrapper">
-	<div id="images">
 <?php
-while (next_album(true))
-{
-?>
-<div class="image">
-	<div class="imagethumb"><a href="<?php echo getAlbumURL();?>" title="<?php echo getAlbumTitle();?>">
-		<?php printSizedAlbumThumbImage(getAlbumTitle()); ?>
-	</a></div>
-	<div class="imagetitle">
-		<h4><a href="<?php echo getAlbumURL();?>" title="<?php echo getAlbumTitle();?>"><?php echo getAlbumTitle(); ?></a></h4>
-		<?php echo printAlbumDesc(); ?>
-	</div>
-</div>
-<?php
-}
-?>
-	</div>
-</div>
-<?php
+    drawIndexAlbums('nodynamic');
 	include_once('footer.php');
 } else {
     include_once('frontpage.php');
