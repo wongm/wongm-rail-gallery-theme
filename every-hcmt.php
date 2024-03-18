2432699c8cc3577fc481f92817e3d37f47c0977b
Marcus Wong <marcusthomaswong@gmail.com>
2024-03-18 20:36:48 +1100

    Add new 'every HCMT page' and add ordering

---
+++ every-hcmt.php
@@ -0,0 +1,9 @@
+<?php $startTime = array_sum(explode(" ",microtime())); if (!defined('WEBPATH')) die(); 
+
+$key = 'hcmt';
+$popularImageText['key'] = $key;
+setOption('photostream_images_per_page', $popularImageText[$key]['maxcount'], false);
+setCustomPhotostream($popularImageText[$key]['where'], "", $popularImageText[$key]['order']);
+
+require_once('uploads-base.php');
+?>
--- functions-link-definitions.php
+++ functions-link-definitions.php
@@ -131,6 +131,7 @@ $popularImageText['nr-class']['title'] = 'Every single NR class locomotive';
 $popularImageText['nr-class']['subtext'] = 'Every single NR class locomotive from NR1 to NR122 (minus NR3 and NR33 because I never photographed them before they were scrapped)';
 $popularImageText['nr-class']['maxcount'] = 122;
 $popularImageText['nr-class']['type'] = 'fleetlist';
+$popularImageText['nr-class']['order'] =  "CONVERT(REPLACE(REGEXP_SUBSTR (i.title, '^*NR+[0-9]*'), 'NR', ''), DECIMAL)";
 $popularImageText['nr-class']['where'] = "i.id IN (SELECT id FROM
 	(
 	select  distinct 
@@ -146,13 +147,13 @@ $popularImageText['nr-class']['where'] = "i.id IN (SELECT id FROM
 	AND zen_albums.folder NOT LIKE '%bits%' AND zen_albums.folder NOT LIKE '%vline%'
 	) as trains
 	WHERE RowNumber = 1 AND locoID <> 0)";
-$popularImageText['nr-class']['order'] =  "CONVERT(REPLACE(REGEXP_SUBSTR (i.title, '^*NR+[0-9]*'), 'NR', ''), DECIMAL)";
 
 $popularImageText['vlocity']['url'] = 'every-vlocity';
 $popularImageText['vlocity']['title'] = 'Every single V/Line VLocity railcar';
 $popularImageText['vlocity']['subtext'] = 'Every single V/Line VLocity railcar from VL00 up to whatever is the current highest number - now VL100 and upwards!';
 $popularImageText['vlocity']['maxcount'] = 300;
 $popularImageText['vlocity']['type'] = 'fleetlist';
+$popularImageText['vlocity']['order'] =  "CONVERT(REPLACE(REGEXP_SUBSTR (REPLACE(REPLACE(i.title, 'VS', 'VL'), 'VLocity', ''), '^*VL+[0-9]*'), 'VL', ''), DECIMAL)";
 $popularImageText['vlocity']['where'] = "i.id IN (SELECT id FROM
 (
 select  distinct 
@@ -170,13 +171,13 @@ AND zen_albums.folder NOT LIKE '%bits%' AND zen_albums.folder NOT LIKE '%interio
 HAVING locoID < 300
 ) as vlocitytrains
 WHERE RowNumber = 1)";
-$popularImageText['vlocity']['order'] =  "CONVERT(REPLACE(REGEXP_SUBSTR (REPLACE(REPLACE(i.title, 'VS', 'VL'), 'VLocity', ''), '^*VL+[0-9]*'), 'VL', ''), DECIMAL)";
 
 $popularImageText['xtrapolis']['url'] = 'every-xtrapolis';
 $popularImageText['xtrapolis']['title'] = 'Every single X\'Trapolis train';
 $popularImageText['xtrapolis']['subtext'] = 'Every single X\'Trapolis train - from 851M to 966M in the first order, then 1M up to 288M and on to 967M-986M in the follow on orders';
 $popularImageText['xtrapolis']['maxcount'] = 600;
 $popularImageText['xtrapolis']['type'] = 'fleetlist';
+$popularImageText['xtrapolis']['order'] =  "CONVERT(REPLACE(REGEXP_SUBSTR (i.title, '^*X''Trapolis +[0-9]*'), 'X''Trapolis ', ''), DECIMAL)";
 $popularImageText['xtrapolis']['where'] = "i.id IN (SELECT id FROM
 (
 select  distinct 
@@ -199,7 +200,6 @@ AND zen_albums.folder NOT LIKE '%new-xtraps%'
 HAVING locoID > 0 AND locoID < 1000
 ) as XTrapolistrains
 WHERE RowNumber = 1)";
-$popularImageText['xtrapolis']['order'] =  "CONVERT(REPLACE(REGEXP_SUBSTR (i.title, '^*X''Trapolis +[0-9]*'), 'X''Trapolis ', ''), DECIMAL)";
 
 $popularImageText['comeng']['url'] = 'every-comeng';
 $popularImageText['comeng']['title'] = 'Every single Comeng train';
@@ -229,11 +229,34 @@ HAVING locoID < 1000
 WHERE RowNumber = 1)";
 $popularImageText['comeng']['order'] =  "CONVERT(REPLACE(REGEXP_SUBSTR (i.title, '^*Comeng +[0-9]*'), 'Comeng ', ''), DECIMAL)";
 
+$popularImageText['hcmt']['url'] = 'every-hcmt';
+$popularImageText['hcmt']['title'] = 'Every single High Capacity Metro Train';
+$popularImageText['hcmt']['subtext'] = 'Every single High Capacity Metro Train - from set 1 through to 70';
+$popularImageText['hcmt']['maxcount'] = 70;
+$popularImageText['hcmt']['type'] = 'fleetlist';
+$popularImageText['hcmt']['order'] =  "CONVERT(REPLACE(REGEXP_SUBSTR (i.title, '^*HCMT set +[0-9]*'), 'HCMT set ', ''), DECIMAL)";
+$popularImageText['hcmt']['where'] = "i.id IN (SELECT id FROM
+(
+select  distinct 
+	i.id,
+	i.title,
+	filename,
+	folder,
+	ROW_NUMBER() OVER (PARTITION BY REGEXP_SUBSTR (i.title, '^*HCMT set {1,3}[0-9]*') ORDER BY i.hitcounter DESC) AS RowNumber,
+	REGEXP_SUBSTR (i.title, '^*HCMT set {1,3}[0-9]M*') AS loco, 
+	CONVERT(REPLACE(REGEXP_SUBSTR (i.title, '^*HCMT set +[0-9]*'), 'HCMT set ', ''), DECIMAL) AS locoID 
+	from zen_images i
+	join zen_albums ON i.albumid = zen_albums.id
+where i.title REGEXP  'HCMT set {1,3}[0-9]'
+) as hcmts
+WHERE RowNumber = 1)";
+
 $popularImageText['e-class']['url'] = 'every-e-class';
 $popularImageText['e-class']['title'] = 'Every single E class tram';
 $popularImageText['e-class']['subtext'] = 'Every single E class tram E.6001 to E2.6100';
 $popularImageText['e-class']['maxcount'] = 122;
 $popularImageText['e-class']['type'] = 'fleetlist';
+$popularImageText['e-class']['order'] =  "CONVERT(REPLACE(REGEXP_SUBSTR (REPLACE(i.title, 'E2.', 'E.'), '^*E.+[0-9]*'), 'E.', ''), DECIMAL)";
 $popularImageText['e-class']['where'] = "i.id IN (SELECT id FROM
 (
 select  distinct 
@@ -251,13 +274,13 @@ AND zen_albums.folder NOT LIKE '%motorists%'
 AND zen_albums.folder NOT LIKE '%preston%'
 ) as nrclasses
 WHERE RowNumber = 1 AND locoID <> 0)";
-$popularImageText['e-class']['order'] =  "CONVERT(REPLACE(REGEXP_SUBSTR (REPLACE(i.title, 'E2.', 'E.'), '^*E.+[0-9]*'), 'E.', ''), DECIMAL)";
 
 $popularImageText['bus']['url'] = 'every-bus';
 $popularImageText['bus']['title'] = 'Every Victorian bus I\'ve photographed';
 $popularImageText['bus']['subtext'] = 'Every single Victorian registered bus that I\'ve photographed - registration plates in the nnnnAO and the BSnnaa series';
 $popularImageText['bus']['maxcount'] = 5000;
 $popularImageText['bus']['type'] = 'fleetlist';
+$popularImageText['bus']['order'] =  "REGEXP_SUBSTR (REPLACE(i.title, 'BS', 'BS99'), 'BS[0-9]{4}[A-Z]*'), CONVERT(REPLACE(REGEXP_SUBSTR (i.title, '[0-9]{4}AO*'), 'AO', ''), DECIMAL)";
 $popularImageText['bus']['where'] = "i.id IN (SELECT id FROM
 (
 select  distinct 
@@ -285,6 +308,5 @@ where (nr.title REGEXP  'BS[0-9]{2}[A-Z]')
 AND zen_albums.folder NOT LIKE '%bits%' 
 ) as nrclasses
 WHERE RowNumber = 1 AND busid <> 0)";
-$popularImageText['bus']['order'] =  "REGEXP_SUBSTR (REPLACE(i.title, 'BS', 'BS99'), 'BS[0-9]{4}[A-Z]*'), CONVERT(REPLACE(REGEXP_SUBSTR (i.title, '[0-9]{4}AO*'), 'AO', ''), DECIMAL)";
 
 ?>
