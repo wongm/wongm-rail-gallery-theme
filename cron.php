ed3850f20796d8033d12e0c661ecbc6e89d1dce8
Marcus Wong <marcusthomaswong@gmail.com>
2024-03-20 11:16:58 +1100

    Cron page fixes for Zenphoto v1.6 and PHP 8 upgrade

--- cron.php
+++ cron.php
@@ -1,62 +1,64 @@
 <?php $startTime = array_sum(explode(" ",microtime())); if (!defined('WEBPATH')) die(); 
 
+global $_zp_db;
+
 if (isset($_GET['token']))
 {
-    if ($_GET['token'] == getOption('wongm_cron_security_key'))
-    {
+	if ($_GET['token'] == getOption('wongm_cron_security_key'))
+	{
         $hiddenCaptionedImageQuery = "SELECT count(1) AS `hiddenUncaptionedImageCount` 
-            FROM " . prefix('images') . " i 
+            FROM " . $_zp_db->prefix('images') . " i 
             WHERE i.`show` = 0 AND " . UNCAPTIONED_IMAGE_REGEX;
             
         $hiddenUncaptionedImageQuery = "SELECT count(1) AS `hiddenCaptionedImageCount` 
-            FROM " . prefix('images') . " i 
+            FROM " . $_zp_db->prefix('images') . " i 
             WHERE i.`show` = 0 AND " . CAPTIONED_IMAGE_REGEX;
         
-        $hiddenUncaptionedImageCount = query_single_row($hiddenCaptionedImageQuery)['hiddenUncaptionedImageCount'];
-        $hiddenCaptionedImageCount = query_single_row($hiddenUncaptionedImageQuery)['hiddenCaptionedImageCount'];
+        $hiddenUncaptionedImageCount = $_zp_db->querySingleRow($hiddenCaptionedImageQuery)['hiddenUncaptionedImageCount'];
+        $hiddenCaptionedImageCount = $_zp_db->querySingleRow($hiddenUncaptionedImageQuery)['hiddenCaptionedImageCount'];
         
         if ($hiddenUncaptionedImageCount == 0 && $hiddenCaptionedImageCount > 0)
         {
-            $updateUnpublishedCaptionedImagesQuery = "UPDATE " . prefix('images') . " i SET i.`show` = 1 WHERE i.`show` = 0 AND " . CAPTIONED_IMAGE_REGEX;
-            query($updateUnpublishedCaptionedImagesQuery);
+            $updateUnpublishedCaptionedImagesQuery = "UPDATE " . $_zp_db->prefix('images') . " i SET i.`show` = 1 WHERE i.`show` = 0 AND " . CAPTIONED_IMAGE_REGEX;
+            $_zp_db->query($updateUnpublishedCaptionedImagesQuery);
             echo "PUBLISHED NEWLY CAPTIONED IMAGES<br>";
             
-            require_once(SERVERPATH . '/' . ZENFOLDER . '/' . PLUGIN_FOLDER . '/static_html_cache.php');
-            static_html_cache::clearHTMLCache();
-            echo "CACHE CLEARED<br>";
+		    require_once(SERVERPATH . '/' . ZENFOLDER . '/' . PLUGIN_FOLDER . '/static_html_cache.php');
+    	    static_html_cache::clearHTMLCache();
+    	    echo "CACHE CLEARED<br>";
         }
         else if ($hiddenUncaptionedImageCount > 0)
         {
             echo "$hiddenUncaptionedImageCount IMAGES NEED CAPTIONS<br>";
-            
-            $getAlbumsToUpdate = "SELECT albumid FROM " . prefix('images') . " GROUP BY albumid HAVING max(DATE(date)) = min(DATE(date))";
-            $results = query_full_array($getAlbumsToUpdate);
-            
-            $albumIds = [];
-            foreach ($results as $albumId)
-            {
-                array_push($albumIds, $albumId['albumid']);
-            }
-            
-            $albumsToSetDates = "UPDATE " . prefix('albums') . " SET sort_type = 'date', image_sortdirection = 0 WHERE id IN (" . implode(",", $albumIds) . ")";
-            query($albumsToSetDates);
-            echo "UPDATED SORTING FOR " . db_affected_rows() . " ALBUMS<BR>";
+			
+			$getAlbumsToUpdate = "SELECT albumid FROM " . prefix('images') . " GROUP BY albumid HAVING max(DATE(date)) = min(DATE(date))";
+			$results = query_full_array($getAlbumsToUpdate);
+			
+			$albumIds = [];
+			foreach ($results as $albumId)
+			{
+				array_push($albumIds, $albumId['albumid']);
+			}
+			
+			$albumsToSetDates = "UPDATE " . $_zp_db->prefix('albums') . " SET sort_type = 'date', image_sortdirection = 0 WHERE id IN (" . implode(",", $albumIds) . ")";
+			$_zp_db->query($albumsToSetDates);
+			echo "UPDATED SORTING FOR " . $_zp_db->dbAffectedRows() . " ALBUMS<BR>";
 
         }
         else
         {
             echo "NO NEW IMAGES TO PUBLISH<br>";
         }
-        
-        if (function_exists('updateHitcounterDates'))
-        {
-            updateHitcounterDates();
-            echo "UPDATE HITCOUNTER<BR>";
-        }
-        
-        echo "DONE!";
-        exit;
-    }
+		
+		if (function_exists('updateHitcounterDates'))
+		{
+			updateHitcounterDates();
+			echo "UPDATE HITCOUNTER<BR>";
+		}
+		
+		echo "DONE!";
+		exit;
+	}
 }
 
 header('HTTP/1.0 403 Forbidden');
