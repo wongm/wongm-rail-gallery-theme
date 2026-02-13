520893c9712f7443d89eb82a193b7645c207e409
Marcus Wong <marcusthomaswong@gmail.com>
2026-02-13 12:51:47 +1100

    Stop https://railgallery.wongm.com/page/archive/%2525252525252525252525252525252525252525252525252 requests from breaking site

--- search.php
+++ search.php
@@ -11,8 +11,20 @@ if (isset($_zp_current_search))
 }
 
 $albumsText = $searchwords = '';
-$totalAlbums = getNumAlbums();
-$totalImages = getNumImages();
+$totalImages = $totalAlbums = 0;
+
+if (isset($_REQUEST['date']) && strlen($_REQUEST['date']) > 11)
+{
+    // do nothing because date value is broken
+    $_zp_current_search = $_REQUEST['date'] = null;
+    include(SERVERPATH . '/' . ZENFOLDER . '/404.php');
+    die();
+}
+else
+{
+    $totalAlbums = getNumAlbums();
+    $totalImages = getNumImages();
+}
 $totalItems = $totalAlbums + $totalImages;
 
 $breadcrumbLinks = '<a href="' . SEARCH_URL_PATH . '" title="Gallery Search">Search</a>';
@@ -23,101 +35,101 @@ $noIndex = true;
 
 if ($totalAlbums > 0)
 {
-	$albumsText = " - $totalAlbums albums and $totalImages images";
+    $albumsText = " - $totalAlbums albums and $totalImages images";
 }
 if ($totalItems > 0)
 {
-	if (isset($_REQUEST['date']))
-	{
-		$joiner = "in";
-		if (sizeof(explode('-', $_REQUEST['date'])) == 3) {
-			$joiner = "on";
-		}
-		
-		$searchwords = getFullSearchDate();
-		$pageTitle = $searchwords;
-		$leadingIntroText = "<h2>Gallery archive</h2>\n";
-		$breadcrumbLinks = '<a href="' . ARCHIVE_URL_PATH . '" title="Gallery Archive">Archive</a> &raquo; ' . $searchwords;
-		$leadingIntroText .= '<p>'.sprintf(gettext('%1$u photos taken %2$s %3$s'), $totalItems, $joiner, $searchwords)."$albumsText.</p>";
-		
-		// but do index pages for a specific date
-		$noIndex = false;
-	}
-	else
-	{
-		$searchwords = getSearchWords();
-		$pageTitle = 'Search results - ' . $searchwords;
-		$leadingIntroText = "<h2>Search results</h2>\n";
-		
-		if ((strlen($searchwords) > 3 || preg_match('/[A-Za-z].*[0-9]|[0-9].*[A-Za-z]/', $searchwords)) && getCurrentPage() == 1)
-		{
-			// but do index search results for 'real' words, but only first page
-			$noIndex = false;
-		}
-	}
+    if (isset($_REQUEST['date']))
+    {
+        $joiner = "in";
+        if (sizeof(explode('-', $_REQUEST['date'])) == 3) {
+            $joiner = "on";
+        }
+        
+        $searchwords = getFullSearchDate();
+        $pageTitle = $searchwords;
+        $leadingIntroText = "<h2>Gallery archive</h2>\n";
+        $breadcrumbLinks = '<a href="' . ARCHIVE_URL_PATH . '" title="Gallery Archive">Archive</a> &raquo; ' . $searchwords;
+        $leadingIntroText .= '<p>'.sprintf(gettext('%1$u photos taken %2$s %3$s'), $totalItems, $joiner, $searchwords)."$albumsText.</p>";
+        
+        // but do index pages for a specific date
+        $noIndex = false;
+    }
+    else
+    {
+        $searchwords = getSearchWords();
+        $pageTitle = 'Search results - ' . $searchwords;
+        $leadingIntroText = "<h2>Search results</h2>\n";
+        
+        if ((strlen($searchwords) > 3 || preg_match('/[A-Za-z].*[0-9]|[0-9].*[A-Za-z]/', $searchwords)) && getCurrentPage() == 1)
+        {
+            // but do index search results for 'real' words, but only first page
+            $noIndex = false;
+        }
+    }
 }
 
 if (strlen($searchwords) == 0)
 {
-	$pageTitle = 'Search';
-	$leadingIntroText = "<h2>Search</h2>";
+    $pageTitle = 'Search';
+    $leadingIntroText = "<h2>Search</h2>";
 }
 
 include_once('header.php');
 
 if (isset($_REQUEST['s'])) { ?>
 <script type="text/javascript">$(document).ready(function() {
-	document.getElementById('search_input').value = '<?php echo str_replace("\"", "", getSearchWords()); ?>';
+    document.getElementById('search_input').value = '<?php echo str_replace("\"", "", getSearchWords()); ?>';
 });</script>
 <?php
 }
  ?>
 <div class="headbar">
-	<span id="breadcrumb">
-		<a href="<?php echo getGalleryIndexURL(); ?>" title="Gallery Index"><?php echo getGalleryTitle(); ?></a> &raquo; <?php echo $breadcrumbLinks; ?>
-	</span>
-	<span id="righthead"><?php echo printSearchForm(); ?></span>
+    <span id="breadcrumb">
+        <a href="<?php echo getGalleryIndexURL(); ?>" title="Gallery Index"><?php echo getGalleryTitle(); ?></a> &raquo; <?php echo $breadcrumbLinks; ?>
+    </span>
+    <span id="righthead"><?php echo printSearchForm(); ?></span>
 </div>
 <div class="topbar">
-	<?php echo $leadingIntroText; ?>
+    <?php echo $leadingIntroText; ?>
 </div>
 <div id="searchpage">
 <?php
 if ($totalAlbums > 0)
 {
-	echo "<div id=\"indexalbums\">\n";
-	$firstAlbum = "";
-	while (next_album())
-	{
-		if (is_null($firstAlbum))
-		{
-			$lastAlbum = albumNumber();
-			$firstAlbum = $lastAlbum;
-		}
-		else
-		{
-			$lastAlbum++;
-		}
-		drawWongmAlbumRow();
-	}
-	echo "</div>";
+    echo "<div id=\"indexalbums\">\n";
+    $firstAlbum = "";
+    while (next_album())
+    {
+        if (is_null($firstAlbum))
+        {
+            $lastAlbum = albumNumber();
+            $firstAlbum = $lastAlbum;
+        }
+        else
+        {
+            $lastAlbum++;
+        }
+        drawWongmAlbumRow();
+    }
+    echo "</div>";
 }
 
 drawWongmGridImages('search', $totalImages); 
 
 if (function_exists('printSlideShowLink')) {
-	echo "<p align=\"center\">";
-	printSlideShowLink(gettext('View Slideshow'));
-	echo "</p>";
+    echo "<p align=\"center\">";
+    printSlideShowLink(gettext('View Slideshow'));
+    echo "</p>";
 }
 if ($totalImages == 0 AND $totalAlbums == 0)
 {
-	echo "<p>".gettext("Sorry, no image matches. Try refining your search.")."</p>";
+    echo "<p>".gettext("Sorry, no image matches. Try refining your search.")."</p>";
 }
 
 if (hasNextPage() OR hasPrevPage())
 {
-	printPageListWithNav("« ".gettext("Prev"), gettext("Next")." »");
+    printPageListWithNav("« ".gettext("Prev"), gettext("Next")." »");
 }
 ?>
 </div>
